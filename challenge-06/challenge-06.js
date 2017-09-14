d4eac723dad4f2ce6f9348685ca80fb191fa0e83
Virginia Cruz <virginiarcruz@gmail.com>
2017-09-14 07:52:24 -0300

    challenge-06 revisao

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,9 +4,12 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-var championship  = 'Campeonato Cearense';
+
+var championship = 'Campeonato Cearense';
 
 console.log(championship);
+
+
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -16,9 +19,9 @@ desafio.
 */
 // ?
 
-var teams = ['Ceara', 'Ferrim', 'Filgueiras', 'Sao Rafa', 'Fortaleza'];
+var teams = ['Ceara', 'Fortaleza', 'Ferroviario', 'Flamengo', 'Santos'];
 
-console.log( 'Times que estão participando do campeonato:', teams );
+
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -36,32 +39,42 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
+
+var teams = ['Ceara', 'Fortaleza', 'Ferroviario', 'Flamengo', 'Santos'];
+
 function showTeamPosition(posicao) {
-    if (posicao < 1 || posicao > 5) {
-        console.log ('O time que está em' + posicao + 'º lugar é o' + teams [posicao -1] + '.');
-    } else {
-        console.log ("Não temos a informação do time que está nessa posição.");
-    }
-}
+  var posicaoReal = posicao - 1;
+  if (posicao >=1 && posicao <= 5 ) {
+    console.log('O time que está em ' + posicao + 'º lugar é o ' + teams[posicaoReal] + '.');
+  } else {
+    console.log('Não temos a informação do time que está nessa posição.');
+  }
+};
+
+
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
+
 showTeamPosition(1);
-showTeamPosition(3);
 showTeamPosition(5);
-showTeamPosition(6);
+showTeamPosition(3);
+showTeamPosition(4);
+
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
+
 var counter = 20;
-while (counter <= 30) {
-    console.log(counter);
-    counter++;
+while(counter <=30) {
+  console.log(counter);
+  counter++;
 }
+
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -75,39 +88,43 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-function convertToHex (cor) {
-    switch (cor) {
-        var hexad;
-        case 'red':
-            var hexad = '#FF0000';
-            break;
-        case 'white':
-            var hexad = '#ffffff';
-            break;
-        case 'black':
-            var hexad = '#0000';
-            break;
-        case 'yellow':
-            var hexad = '#fDDCff';
-            break;
-         case 'gray':
-            var hexad = '#d4d4d4';
-            break;
-        default : 
-            console.log("Não temos o equivalente hexadecimal para " + cor + ".");
-    }
-    console.log ('O hexadecimal para a cor ' + cor + " é " + hexad + ".");
 
+function convertToHex(cor) {
+  switch (cor) {
+    case 'Vermelho' :
+      var hexa = '#ff0000';
+      break;
+    case 'Azul' :
+      var hexa = '#123445';
+      break;
+    case 'Preto' :
+      var hexa = '#000000'
+      break;
+    case 'Branco' :
+      var hexa = '#ffffff';
+      break;
+    case 'Amarelo' :
+      var hexa = '#654321';
+      break;
+    default :
+      console.log('Não temos o equivalente hexadecimal para [COR].');
+  }
+  console.log('O hexadecimal para a cor ' + cor +  ' é ' + hexa + '.');
 }
+
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-convertToHex('red');
-convertToHex('gray');
-convertToHex('green');
-convertToHex('yellow');
-convertToHex('black');
-convertToHex('blue');
-convertToHex('white');
-convertToHex('purple');
+
+convertToHex('Vermelho');
+convertToHex('Azul');
+convertToHex('Preto');
+convertToHex('Preto');
+convertToHex('Branco');
+convertToHex('Amarelo');
+convertToHex('Verde');
+convertToHex('Rosa');
+
+
+// Revisao
