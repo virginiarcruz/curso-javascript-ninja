f122c6da7ebf1a1e9c667f7e77d4c6748d45f3f7
Virginia Cruz <virginiarcruz@gmail.com>
2017-09-19 08:24:12 -0300

    challenge-08 revisao

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,46 +3,56 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-var sum = function calculateSum (x,y) {
-  return  x + y ;
-};
+
+var sum = function calculateSum (x, y) {
+  return x + y;
+}
+
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-var val1 = 20;
-var val2 = 50;
+var val1 = 2;
+var val2 = 4;
+console.log('A soma de ' + val1 +  ' e ' +  val2 +  ' é igual a'  + sum(val1,val2) + '.')
 
-console.log("A soma de " + val1 + " e " + val2 + "é igual a " + 
- sum(val1,val2) );
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-console.log("O nome da função que faz a soma é " +sum.name + ".");
+
+console.log('O nome da função que faz a soma é ' +  sum.name + '.')
+
+
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
+
 function showName() {
-  return "Virginia Rodrigues";
+  return 'Virginia Rodrigues';
 }
+
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
+
 var varShowName = showName;
+
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-console.log("A função " + varShowName.name + " retorna " + varShowName()+ " .");
+
+console.log('A função ' + showName.name +  ' retorna ' + varShowName() + '.');
+
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -56,29 +66,31 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-function calculator (operator) {
-  var result;
-  switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = num1 / num2;
-      break;
-    case '%':
-      result = num1 % num2;
-      break;
-    default: 
-      return 'operador inválido';
-  }
-  return function ( num1, num2) {
-        return "Resultado da operação: " + num1 + "" + operator + "" + num2 + "=" + result + "." ;
+
+
+function calculator( operacao ) {
+  return function (num1, num2) {
+    var result;
+      switch (operacao) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num1 / num2;
+          break;
+        case '%':
+          result = num1 % num2;
+          break;
+        default :
+          return 'Operação Inválida';
+      }
+    console.log('Resultado da operação:' +  num1 +  operacao + num2 + ' = ' + result + ' .');
   };
 }
 
@@ -86,28 +98,36 @@ function calculator (operator) {
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
+
 var sum = calculator('+');
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-sum(9,78);
+
+sum(10,20);
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
+
 var subtraction = calculator('-');
 var multiplication = calculator('*');
 var division = calculator('/');
 var mod = calculator('%');
 
+
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-console.log(subtraction(90,10));  
-console.log(multiplication(90,10));  
-console.log(division(90,10));  
-console.log(mod(90,10));  
+
+console.log( subtraction(20,10));
+console.log( multiplication(20,5));
+console.log( division(20,2));
+console.log( mod(30,2));
+
+
+// Revisado
